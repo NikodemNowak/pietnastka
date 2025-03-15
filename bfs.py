2be5c6b6df6b82bc8863d8aecc2178362ee72c5e
NikodemNowak <71512463+NikodemNowak@users.noreply.github.com>
2025-03-15 18:11:31 +0100

    Refactor BFS and board functions; improve state management and movement logic

--- bfs.py
+++ bfs.py
@@ -1,51 +1,84 @@
+from queue import Queue
+
 from board import *
 
 visited = set()
+all_layer_states = Queue()
 
-def bfs(order, board):
+def bfs(order, board, rows, cols):
 
-    y, x = where_zero(board)
-    layer_moves = []
+    is_in_set(hash(board))
+
+    temporary_bfs(order, board, rows, cols)
+
+    while not all_layer_states.empty():
+        b = all_layer_states.get()
+        temporary_bfs(order, b, rows, cols)
+
+    print("dlugosc visited: " + str(len(visited)))
+
+def temporary_bfs(order, board, rows, cols):
+    x = where_zero(board)
+    board_moves = []
     last_step = -1
 
     # Które ruchy są możliwe
-    check_possible_move(x, y , last_step, len(board[0]), len(board), layer_moves)
+    check_possible_move(x, last_step, cols, rows, board_moves)
 
     # Posortuje możliwe ruchy zgodnie z order
-    layer_moves = sorted(layer_moves, key=lambda z: order.index(z))
-    print(layer_moves)
+    board_moves = sorted(board_moves, key=lambda z: order.index(z))
 
+    # for letter in order:
+    #     if letter in layer_moves:
+    #         print(letter)
 
+    for move in board_moves:
+        if move == 'L':
+            new_board = move_left(board)
+        elif move == 'U':
+            new_board = move_up(board)
+        elif move == 'R':
+            new_board = move_right(board)
+        elif move == 'D':
+            new_board = move_down(board)
 
+        # print(new_board)
+        # print(hash_board(new_board))
+        # print(check_board(new_board))
+        # print(where_zero(new_board))
 
-def check_possible_move(x, y, last_step, width, height, layer_moves):
-    if left_ok(x, last_step):
+        if is_in_set(hash_board(new_board)):
+            all_layer_states.put(new_board)
+            print("New board added to queue: " + str(new_board))
+
+def check_possible_move(x, last_step, width, height, layer_moves):
+    if left_ok(x, last_step, width):
         layer_moves.append('L')
 
-    if up_ok(y, last_step):
+    if up_ok(x, last_step, width):
         layer_moves.append('U')
 
     if right_ok(x, last_step, width):
         layer_moves.append('R')
 
-    if down_ok(y, last_step, height):
+    if down_ok(x, last_step, width, height):
         layer_moves.append('D')
     return layer_moves
 
-def left_ok(x, last_step):
+def left_ok(x, last_step, width):
     if last_step == 3:
         return False
 
-    if x == 0:
+    if x % width == 0:
         return False
 
     return True
 
-def up_ok(y, last_step):
+def up_ok(x, last_step, width):
     if last_step == 6:
         return False
 
-    if y == 0:
+    if x < width:
         return False
 
     return True
@@ -54,16 +87,16 @@ def right_ok(x, last_step, width):
     if last_step == 9:
         return False
 
-    if x == width - 1:
+    if x % width == width - 1:
         return False
 
     return True
 
-def down_ok(y, last_step, height):
+def down_ok(x, last_step, width, height):
     if last_step == 12:
         return False
 
-    if y == height - 1:
+    if x >= (width - 1) * height:
         return False
 
     return True
@@ -71,6 +104,8 @@ def down_ok(y, last_step, height):
 def is_in_set(hash_param):
     if hash_param not in visited:
         visited.add(hash_param)
-        print(str(hash_param) + " added to set")
+        # print(str(hash_param) + " added to set")
+        return True
     else:
-        print(str(hash_param) + " already in set")
+        # print(str(hash_param) + " already in set")
+        return False
