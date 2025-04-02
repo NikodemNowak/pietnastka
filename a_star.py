09e3e70a8772944c23ed36166b3c0e2a5155a020
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2025-04-02 16:46:14 +0200

    Enhance A* algorithm implementation with detailed heuristic function documentation and improved board state management

--- a_star.py
+++ a_star.py
@@ -1,11 +1,31 @@
-from temporary_algorithm import *
-from board import *
+from queue import PriorityQueue
+from temporary_algorithm import temporary_algorithm
+from board import check_board, get_rows, get_cols
+
 
 def hamming_distance(board):
+    """
+    Funkcja heurystyczna obliczająca odległość Hamminga (liczbę kafelków nie na swoim miejscu).
+
+    Args:
+        board: Układ planszy jako krotka
+
+    Returns:
+        int: Liczba kafelków nie na właściwym miejscu
+    """
     return check_board(board)
 
 
 def manhattan_distance(board):
+    """
+    Funkcja heurystyczna obliczająca odległość Manhattan dla wszystkich kafelków.
+
+    Args:
+        board: Układ planszy jako krotka
+
+    Returns:
+        int: Suma odległości Manhattan dla wszystkich kafelków
+    """
     distance = 0
     rows = get_rows()
     cols = get_cols()
@@ -30,33 +50,54 @@ def manhattan_distance(board):
 
 
 def a_star(heuristics, board_dict):
+    """
+    Implementacja algorytmu A* do rozwiązywania układanki.
 
+    Args:
+        heuristics: Funkcja heurystyczna używana do oceny stanów
+        board_dict: Słownik reprezentujący początkowy stan planszy
+
+    Returns:
+        tuple: (czy znaleziono, stan końcowy, sekwencja ruchów, odwiedzone stany, liczba przetworzonych stanów, maksymalna głębokość)
+    """
+    # Inicjalizacja struktur danych
     queue_a_star = PriorityQueue()
     visited_a_star = set()
     processed_a_star = 0
-    additional_counter = [0] # Aby było mutowalne
+    additional_counter = [0]  # Lista, aby licznik był mutowalny przez referencję - służy do rozstrzygania remisów
     max_depth = 20
     max_processed_depth = 0
 
-    priority = heuristics(next(iter(board_dict.keys()))) + len(list(board_dict.values())[0]) - 1
+    # Obliczenie początkowego priorytetu (f = g + h)
+    # g = koszt dotychczasowej ścieżki (długość kroków - 1)
+    # h = wartość funkcji heurystycznej
+    board = next(iter(board_dict.keys()))
+    steps = list(board_dict.values())[0]
+    priority = heuristics(board) + len(steps) - 1
 
-    visited_a_star.add(next(iter(board_dict.keys())))
-    queue_a_star.put((priority, additional_counter[0], board_dict))  # Dodaje do kolejki z priorytetem
+    # Dodanie stanu początkowego do zbioru odwiedzonych i kolejki priorytetowej
+    visited_a_star.add(board)
+    queue_a_star.put((priority, additional_counter[0], board_dict))
     additional_counter[0] += 1
 
     while not queue_a_star.empty():
-
-        prio, _, b = queue_a_star.get()  # SYPIE BŁĘDEM, BO JAK HEURYSTYKA DA TO SAMO TO NIE UMIE POSOROTWAĆ
+        # Pobranie stanu o najniższym priorytecie
+        # Drugi element (counter) zapewnia deterministyczne zachowanie przy równych priorytetach
+        _, _, b = queue_a_star.get()
         processed_a_star += 1
 
+        # Aktualizacja informacji o głębokości przeszukiwania
         current_depth = len(list(b.values())[0]) - 1
         if current_depth > max_processed_depth:
             max_processed_depth = current_depth
 
-        if check_board(next(iter(b.keys()))) == 0:
-            return True, str(next(iter(b.keys()))), str(list(b.values())[0][1:]), visited_a_star, processed_a_star, max_processed_depth
+        # Sprawdzenie czy osiągnięto stan docelowy
+        board = next(iter(b.keys()))
+        if check_board(board) == 0:
+            return True, str(board), str(list(b.values())[0][1:]), visited_a_star, processed_a_star, max_processed_depth
 
+        # Generowanie kolejnych stanów
         temporary_algorithm(heuristics, b, queue_a_star, visited_a_star, additional_counter, False)
 
-
+    # W przypadku niepowodzenia
     return False, '', '-1', visited_a_star, processed_a_star, max_depth
--- board.py
+++ board.py
@@ -1,18 +1,29 @@
-import time
+# Zmienne globalne dotyczące planszy
+f = ''  # Uchwyt do pliku
+rows, cols = 0, 0  # Wymiary planszy: liczba wierszy i kolumn
 
-f = ''
-rows, cols = 0, 0
 
 def get_rows():
+    """Zwraca liczbę wierszy planszy."""
     return rows
+
+
 def get_cols():
+    """Zwraca liczbę kolumn planszy."""
     return cols
 
-# Wczytuje planszę z pliku
+
 def load_board(path):
-    global f
-    global rows
-    global cols
+    """
+    Wczytuje planszę z pliku.
+
+    Args:
+        path: Ścieżka do pliku z danymi planszy
+
+    Returns:
+        tuple: Krotka zawierająca (plansza, liczba wierszy, liczba kolumn)
+    """
+    global f, rows, cols
     f = open(path, "r")
     rows, cols = map(int, f.readline().split())
     arr = []
@@ -24,106 +35,227 @@ def load_board(path):
 
     return tuple(arr), rows, cols
 
-# Generuje finalną planszę
+
 def generate_final_board():
+    """
+    Generuje finalną planszę, gdzie liczby są ułożone po kolei, a 0 jest na końcu.
+
+    Returns:
+        tuple: Krotka reprezentująca stan końcowy planszy
+    """
     i = 1
     final_board = []
-    for _ in range(rows*cols):
+    for _ in range(rows * cols):
         final_board.append(i)
         i += 1
-    final_board[rows*cols-1] = 0
+    final_board[rows * cols - 1] = 0
     return tuple(final_board)
 
-# Sprawdza czy plansza jest rozwiazana
+
 def check_board(board):
+    """
+    Sprawdza, ile pól planszy różni się od stanu końcowego.
+
+    Args:
+        board: Stan planszy do sprawdzenia
+
+    Returns:
+        int: Liczba różniących się pól
+    """
     final_board = generate_final_board()
     x = 0
     for i in range(rows):
         for j in range(cols):
-            x += board[j+i*rows] != final_board[j+i*rows]
+            x += board[j + i * rows] != final_board[j + i * rows]
     return x
 
 
-# Zwraca hash planszy
 def hash_board(board):
+    """
+    Hash planszy.
+
+    Args:
+        board: Stan planszy
+
+    Returns:
+        int: Hash planszy
+    """
     return hash(board)
 
-# Znajduje pustą komórkę
+
 def where_zero(board):
-    for i in range(rows*cols):
+    """
+    Znajduje indeks pustej komórki (oznaczonej jako 0) na planszy.
+
+    Args:
+        board: Stan planszy
+
+    Returns:
+        int: Indeks komórki z zerem
+    """
+    for i in range(rows * cols):
         if board[i] == 0:
             return i
 
-# Przesuwa pustą komórkę w lewo
+
+
 def move_left(board):
+    """
+    Przesuwa pustą komórkę w lewo.
+
+    Args:
+        board: Aktualny stan planszy
+
+    Returns:
+        tuple: (nowy stan planszy, symbol ruchu)
+    """
     x = where_zero(board)
     temp_board = list(board)
-    temp_board[x], temp_board[x-1] = board[x-1], board[x]
+    temp_board[x], temp_board[x - 1] = board[x - 1], board[x]
     return tuple(temp_board), 'L'
 
-# Przesuwa pustą komórkę w prawo
+
 def move_right(board):
+    """
+    Przesuwa pustą komórkę w prawo.
+
+    Args:
+        board: Aktualny stan planszy
+
+    Returns:
+        tuple: (nowy stan planszy, symbol ruchu)
+    """
     x = where_zero(board)
     temp_board = list(board)
-    temp_board[x], temp_board[x+1] = board[x+1], board[x]
+    temp_board[x], temp_board[x + 1] = board[x + 1], board[x]
     return tuple(temp_board), 'R'
 
-# Przesuwa pustą komórkę w górę
+
 def move_up(board):
+    """
+    Przesuwa pustą komórkę w górę.
+
+    Args:
+        board: Aktualny stan planszy
+
+    Returns:
+        tuple: (nowy stan planszy, symbol ruchu)
+    """
     x = where_zero(board)
     temp_board = list(board)
-    temp_board[x], temp_board[x-cols] = board[x-cols], board[x]
+    temp_board[x], temp_board[x - cols] = board[x - cols], board[x]
     return tuple(temp_board), 'U'
 
-# Przesuwa pustą komórkę w dół
+
 def move_down(board):
+    """
+    Przesuwa pustą komórkę w dół.
+
+    Args:
+        board: Aktualny stan planszy
+
+    Returns:
+        tuple: (nowy stan planszy, symbol ruchu)
+    """
     x = where_zero(board)
     temp_board = list(board)
-    temp_board[x], temp_board[x+cols] = board[x+cols], board[x]
+    temp_board[x], temp_board[x + cols] = board[x + cols], board[x]
     return tuple(temp_board), 'D'
 
-# Sprawdza czy można iść w lewo
+
+
 def left_ok(x, last_step):
-    if last_step == 'R':
+    """
+    Sprawdza czy można przesunąć pustą komórkę w lewo.
+
+    Args:
+        x: Indeks pustej komórki
+        last_step: Ostatni wykonany ruch
+
+    Returns:
+        bool: Czy ruch jest możliwy
+    """
+    if last_step == 'R':  # Nie pozwalamy na cofnięcie ostatniego ruchu
         return False
 
-    if x % cols == 0:
+    if x % cols == 0:  # Sprawdza czy komórka jest na lewej krawędzi
         return False
 
     return True
 
-# Sprawdza czy można iść w górę
+
 def up_ok(x, last_step):
-    if last_step == 'D':
+    """
+    Sprawdza czy można przesunąć pustą komórkę w górę.
+
+    Args:
+        x: Indeks pustej komórki
+        last_step: Ostatni wykonany ruch
+
+    Returns:
+        bool: Czy ruch jest możliwy
+    """
+    if last_step == 'D':  # Nie pozwalamy na cofnięcie ostatniego ruchu
         return False
 
-    if x < cols:
+    if x < cols:  # Sprawdza czy komórka jest w pierwszym wierszu
         return False
 
     return True
 
-# Sprawdza czy można iść w prawo
+
 def right_ok(x, last_step):
-    if last_step == 'L':
+    """
+    Sprawdza czy można przesunąć pustą komórkę w prawo.
+
+    Args:
+        x: Indeks pustej komórki
+        last_step: Ostatni wykonany ruch
+
+    Returns:
+        bool: Czy ruch jest możliwy
+    """
+    if last_step == 'L':  # Nie pozwalamy na cofnięcie ostatniego ruchu
         return False
 
-    if x % cols == cols - 1:
+    if x % cols == cols - 1:  # Sprawdza czy komórka jest na prawej krawędzi
         return False
 
     return True
 
-# Sprawdza czy można iść w dół
+
 def down_ok(x, last_step):
-    if last_step == 'U':
+    """
+    Sprawdza czy można przesunąć pustą komórkę w dół.
+
+    Args:
+        x: Indeks pustej komórki
+        last_step: Ostatni wykonany ruch
+
+    Returns:
+        bool: Czy ruch jest możliwy
+    """
+    if last_step == 'U':  # Nie pozwalamy na cofnięcie ostatniego ruchu
         return False
 
-    if x >= (cols - 1) * rows:
+    if x >= (cols - 1) * rows:  # Sprawdza czy komórka jest w ostatnim wierszu
         return False
 
     return True
 
-# Sprawdza które ruchy są możliwe
+
 def check_possible_move(x, last_step, layer_moves):
+    """
+    Sprawdza które ruchy są możliwe i dodaje je do listy.
+
+    Args:
+        x: Indeks pustej komórki
+        last_step: Ostatni wykonany ruch
+        layer_moves: Lista możliwych ruchów
+
+    Returns:
+        list: Zaktualizowana lista możliwych ruchów
+    """
     if left_ok(x, last_step):
         layer_moves.append('L')
 
@@ -137,24 +269,48 @@ def check_possible_move(x, last_step, layer_moves):
         layer_moves.append('D')
     return layer_moves
 
-# Sprawdza czy dany stan jest w zbiorze odwiedzonych
+
+
+
 def is_in_set(dict_param, visited):
+    """
+    Sprawdza czy dany stan jest w zbiorze odwiedzonych.
+    Jeśli nie, dodaje go do zbioru.
+
+    Args:
+        dict_param: Stan do sprawdzenia
+        visited: Zbiór odwiedzonych stanów
+
+    Returns:
+        bool: True jeśli stan nie był wcześniej w zbiorze, False w przeciwnym przypadku
+    """
     if dict_param not in visited:
         visited.add(dict_param)
         return True
     else:
         return False
 
-def add_to_set_dfs(dict_param, visited_chainmap):
 
+def add_to_set_dfs(dict_param, visited_chainmap):
+    """
+    Dodaje stan do zbioru odwiedzonych dla algorytmu DFS.
+    Jeśli stan już istnieje, zachowuje krótszą ścieżkę.
+
+    Args:
+        dict_param: Słownik {plansza: ścieżka}
+        visited_chainmap: ChainMap z odwiedzonymi stanami
+
+    Returns:
+        bool: True jeśli dodano nowy stan lub zaktualizowano ścieżkę, False gdy zachowano starą ścieżkę
+    """
     board = list(dict_param.keys())[0]
     path = dict_param[board]
 
     # Sprawdza czy dany stan jest w ChainMap
     if board in visited_chainmap:
-        # Jeśli długość ścieżki jest mniejsza to zamienia
+        # Jeśli długość ścieżki jest mniejsza, to zamienia
         if len(path) < len(visited_chainmap[board]):
-            # Musimy znaleźć, w którym słowniku jest plansza
+            # Znajduje słownik, w którym jest plansza i aktualizuje ścieżkę
             for d in visited_chainmap.maps:
                 if board in d:
                     d[board] = path
@@ -167,7 +323,20 @@ def add_to_set_dfs(dict_param, visited_chainmap):
         visited_chainmap.maps[0][board] = path
         return True
 
+
 def summary_info(is_ok, final_board, steps, visited, processed, max_processed_depth, time_taken):
+    """
+    Wyświetla podsumowanie wykonania algorytmu.
+
+    Args:
+        is_ok: Czy znaleziono rozwiązanie
+        final_board: Końcowy stan planszy
+        steps: Liczba kroków lub sekwencja kroków
+        visited: Zbiór odwiedzonych stanów
+        processed: Liczba przetworzonych stanów
+        max_processed_depth: Maksymalna głębokość przetworzonych stanów
+        time_taken: Czas wykonania w sekundach
+    """
     if is_ok:
         print("Znaleziono rozwiązanie: " + final_board + " z krokiem: " + steps)
         print("Ilość kroków: " + str(len(steps)))
@@ -178,5 +347,5 @@ def summary_info(is_ok, final_board, steps, visited, processed, max_processed_de
     print("Ilość odwiedzonych stanów: " + str(len(visited)))
     print("Ilość przetworzonych stanów: " + str(processed))
     print("Maksymalna głębokość przetworzonych stanów: " + str(max_processed_depth))
-    print(f"Czas wykonania: {time_taken*1000:.3f} milisekund")
+    print(f"Czas wykonania: {time_taken * 1000:.3f} milisekund")
     return
